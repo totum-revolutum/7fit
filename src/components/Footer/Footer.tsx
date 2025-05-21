def55cb6e027b75feb9aa27cd9e51af800fd6be9
Olga Brendak <olga.brendak.dg@gmail.com>
2025-05-21 17:55:48 +0300

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,27 +1,26 @@
 import styles from "./Footer.module.scss";
-import {
-  CONTACTS,
-  NAVIGATION_LINKS,
-  CLIENT_LINKS,
-  SOCIAL_LINKS,
-} from "../../constants/footer";
+import { CONTACTS, SOCIAL_LINKS } from "../../constants/footer";
 
 export const Footer = () => {
   return (
     <footer id="contacts" className={styles.footer}>
       <div className={styles.footer__container}>
-        <div className={styles.footer__section}>
+        <div className={`${styles.footer__section} ${styles.logoSection}`}>
           <a href="/" className={styles.footer__logo}>
-            Sevenfit EMS
+            <img
+              src="/images/logo/logo124.svg"
+              className={styles.footer__img}
+            />
           </a>
-          <p className={styles.footer__desc}>
-            Твій шлях до ідеального тіла з технологією EMS
-          </p>
+          <div className={styles.footer__desc}>
+            Твій шлях до ідеального тіла з технологією
+            <span className={styles.highlight}> EMS</span>
+          </div>
         </div>
 
         <div className={styles.footer__section}>
-          <h4>Контакти</h4>
-          <ul>
+          {/* <h4>Контакти</h4> */}
+          <ul className={styles.footer__section__info}>
             {CONTACTS.map((item, index) => (
               <li key={index}>{item}</li>
             ))}
@@ -29,40 +28,34 @@ export const Footer = () => {
         </div>
 
         <div className={styles.footer__section}>
-          <h4>Навігація</h4>
-          <ul>
-            {NAVIGATION_LINKS.map((link, index) => (
-              <li key={index}>
-                <a href={link.href}>{link.label}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className={styles.footer__section}>
-          <h4>Клієнтам</h4>
-          <ul>
-            {CLIENT_LINKS.map((link, index) => (
-              <li key={index}>
-                <a href={link.href}>{link.label}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className={styles.footer__section}>
-          <h4>Ми в соцмережах</h4>
+          {/* <div >Соціальні мережі</div> */}
           <div className={styles.footer__socials}>
             {SOCIAL_LINKS.map((social, index) => (
               <a href={social.href} key={index} target="_blank">
-                <img src={social.icon} alt={social.name} />
+                <img
+                  src={social.icon}
+                  alt={social.name}
+                  className={styles.footer__socials__link}
+                />
               </a>
             ))}
           </div>
         </div>
+
+        <div className={`${styles.footer__section} ${styles.map}`}>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1508.9899097504747!2d30.51888381375355!3d50.50052342317652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4d39536567fd7%3A0x30dc4c7e26b93c3a!2zU2V2ZW4gRml0IC0g0JXQnNChINCi0YDQtdC90YPQstCw0L3QvdGPINC90LAg0J7QsdC-0LvQvtC90ZY!5e0!3m2!1sru!2sru!4v1747055453766!5m2!1sru!2sru"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </div>
       </div>
 
-      <div className={styles.footer__bottom}>
+      <div className={styles.footer__allRight}>
         <p>© 2025 Sevenfit EMS. Всі права захищені.</p>
       </div>
     </footer>
