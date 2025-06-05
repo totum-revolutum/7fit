f122c36b265ec09c16d50458a7fade2c9f6eb6ee
Olga Brendak <olga.brendak.dg@gmail.com>
2025-06-05 17:44:16 +0300

    fix footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,34 +1,25 @@
 import styles from "./Footer.module.scss";
-import { CONTACTS, SOCIAL_LINKS } from "../../constants/footer";
+import { Link } from "react-router-dom";
+import { CONTACTS, SOCIAL_LINKS } from "@constants/footer";
 
 export const Footer = () => {
   return (
     <footer id="contacts" className={styles.footer}>
       <div className={styles.footer__container}>
         <div className={`${styles.footer__section} ${styles.logoSection}`}>
-          <a href="/" className={styles.footer__logo}>
+          <Link to="/" className={styles.footer__logo}>
             <img
+            alt="Логотип"
               src="/images/logo/logo124.svg"
               className={styles.footer__img}
             />
-          </a>
+          </Link>
+
           <div className={styles.footer__desc}>
             Твій шлях до ідеального тіла з технологією
             <span className={styles.highlight}> EMS</span>
           </div>
-        </div>
 
-        <div className={styles.footer__section}>
-          {/* <h4>Контакти</h4> */}
-          <ul className={styles.footer__section__info}>
-            {CONTACTS.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul>
-        </div>
-
-        <div className={styles.footer__section}>
-          {/* <div >Соціальні мережі</div> */}
           <div className={styles.footer__socials}>
             {SOCIAL_LINKS.map((social, index) => (
               <a href={social.href} key={index} target="_blank">
@@ -42,9 +33,17 @@ export const Footer = () => {
           </div>
         </div>
 
+        <div className={styles.footer__section}>
+          <ul className={styles.footer__section__info}>
+            {CONTACTS.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+
         <div className={`${styles.footer__section} ${styles.map}`}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1508.9899097504747!2d30.51888381375355!3d50.50052342317652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4d39536567fd7%3A0x30dc4c7e26b93c3a!2zU2V2ZW4gRml0IC0g0JXQnNChINCi0YDQtdC90YPQstCw0L3QvdGPINC90LAg0J7QsdC-0LvQvtC90ZY!5e0!3m2!1sru!2sru!4v1747055453766!5m2!1sru!2sru"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1508.9899097504747!2d30.51888381375355!3d50.50052342317652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4d39536567fd7%3A0x30dc4c7e26b93c3a!2zU2V2ZW4gRml0IC0g0JXQnNChINCi0YDQtdC90YPQstCw0L3QvdGPINC90LAg0J7QsdC-0LvQvtC90ZY!5e0!3m2!1suk!2suk!4v1747055453766!5m2!1suk!2suk"
             width="100%"
             height="100%"
             style={{ border: 0 }}
