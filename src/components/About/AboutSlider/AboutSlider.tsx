0020aaf9ddaad76743e3f48a071c3bfde3272f08
Olga Brendak <olga.brendak.dg@gmail.com>
2025-07-08 11:51:24 +0300

    fix slider

--- src/components/About/AboutSlider/AboutSlider.tsx
+++ src/components/About/AboutSlider/AboutSlider.tsx
@@ -1,4 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useEffect } from "react";
 import { Navigation, Pagination } from "swiper/modules";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
@@ -8,29 +9,38 @@ import "swiper/css/pagination";
 import { images } from "@constants/about";
 import "../AboutSlider/AboutSlider.scss";
 
-const AboutSlider = () => (
-  <div className="sliderWrapper">
-    <Swiper
-      modules={[Navigation, Pagination, Autoplay]}
-      cssMode={true}
-      autoplay={{
-        delay: 3000,
-        disableOnInteraction: false,
-      }}
-      navigation={true}
-      pagination={{ clickable: true }}
-      slidesPerView={1}
-      centeredSlides={true}
-      loop={true}
-      className="swiper"
-    >
-      {images.map((img, index) => (
-        <SwiperSlide key={index}>
-          <img src={img.src} alt={img.alt} />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </div>
-);
+import { useImageStore } from "@/stores/imageStore";
+
+const AboutSlider = () => {
+  const { ems, fetchEmsImg } = useImageStore();
+
+  useEffect(() => {
+    fetchEmsImg();
+  }, []);
+  return (
+    <div className="sliderWrapper">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        cssMode={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        pagination={{ clickable: true }}
+        slidesPerView={1}
+        centeredSlides={true}
+        loop={true}
+        className="swiper"
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <img src={img.src} alt={img.alt} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
 
 export default AboutSlider;
